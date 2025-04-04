2e6025b4ac770e7e04869794487331c151d20bf3
Dhanshri Pawade <pd8806562875@gmail.com>
2025-04-04 11:31:36 +0530

    create file store.js

---
+++ src/stores/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./Products/productSlice";
+
+import useReducer  from "./user/userSlice";
+import blogReducer from "./blog/blogSlice"
+
+
+const store = configureStore({
+  reducer: {
+    product: productReducer,
+    user : useReducer,
+    blog: blogReducer, 
+
+  },
+});
+
+export default store;
